cbc557a048beacf99c7a340c92ed2ef90c4b97ef
Emran Hossain <emranffl.biz@gmail.com>
2024-11-15 19:56:01 +0600

    refactor og-screenshot API to use puppeteer-core and chrome-aws-lambda for improved compatibility and performance

--- app/api/og-screenshot/route.ts
+++ app/api/og-screenshot/route.ts
@@ -1,26 +1,46 @@
+import chrome from "chrome-aws-lambda"
 import { NextRequest, NextResponse } from "next/server"
-import { chromium } from "playwright"
-
-export const config = {
-  runtime: "nodejs",
-}
+import puppeteer from "puppeteer-core"
 
 export async function GET(req: NextRequest) {
   // Launch the Playwright browser
-  const browser = await chromium.launch({ headless: true })
+  const options = process.env.AWS_REGION
+    ? {
+        args: chrome.args,
+        executablePath: await chrome.executablePath,
+        headless: chrome.headless,
+      }
+    : {
+        args: [],
+        executablePath:
+          process.platform === "win32"
+            ? "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+            : process.platform === "linux"
+              ? "/usr/bin/google-chrome"
+              : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+      }
+  const browser = await puppeteer.launch(options)
   const page = await browser.newPage()
   const view = req.nextUrl.searchParams.get("view") || "/"
 
   // Navigate to the specified URL
   await page.goto(process.env.NEXT_PUBLIC_APP_URL! + view, {
-    waitUntil: "networkidle",
+    waitUntil: "networkidle2",
   })
 
   // Set viewport dimensions
-  await page.setViewportSize({ width: 1200, height: 630 })
+  await page.setViewport({ width: 1200, height: 630, deviceScaleFactor: 1, isMobile: false })
 
   // Capture the screenshot as a buffer
-  const imageBuffer = await page.screenshot({ type: "jpeg" })
+  const imageBuffer = (await page.screenshot({
+    type: "jpeg",
+    clip: {
+      width: 1200,
+      height: 630,
+      x: 0,
+      y: 0,
+    },
+  })) as Buffer
 
   // Close the browser
   await browser.close()
