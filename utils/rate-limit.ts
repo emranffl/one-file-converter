450bfa1c8c3ed7d03fdac03e18086fc813f27d77
Emran Hossain <emranffl.biz@gmail.com>
2025-04-05 15:44:34 +0600

    refactor rate limit key generation to include app name abbreviation and improve code readability

--- utils/rate-limit.ts
+++ utils/rate-limit.ts
@@ -2,20 +2,32 @@ import { CONSTANTS } from "@/lib/constants"
 import { Redis } from "@upstash/redis"
 import { ipAddress } from "@vercel/functions"
 import { NextRequest } from "next/server"
-
 const redis = Redis.fromEnv()
+
+const extractAbbreviation = (input: string) => {
+  input = input.toLowerCase().replace(/\s+/g, " ")
+  const reg = /[\s\-_.,;:]/
+
+  input = reg.test(input)
+    ? input
+        .split(reg)
+        .filter((p) => p)
+        .map((p) => p[0])
+        .join("")
+        .substring(0, 3)
+    : input.substring(0, 3)
+
+  return input
+}
 
 export async function rateLimit(request: NextRequest) {
   const ip = ipAddress(request) ?? "127.0.0.1"
-  const key = `rate-limit:${ip}`
+  const key = `${extractAbbreviation(process.env.NEXT_PUBLIC_APP_NAME!)}-rate-limit:${ip}`
 
   const [response] = await redis.pipeline().incr(key).expire(key, CONSTANTS.RATE_LIMIT.WINDOW).exec()
 
   const currentRequests = response as number
-
-  if (currentRequests > CONSTANTS.RATE_LIMIT.MAX) {
-    return false
-  }
+  if (currentRequests > CONSTANTS.RATE_LIMIT.MAX) return false
 
   return true
 }
